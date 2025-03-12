f12ca9f1a55db44eaf172673e4bf9f120c19bae2
Gabriel Felix <gabafp@hotmail.com>
2025-03-12 15:50:02 -0400

    modified:   django_wc/wc_app/urls.py 	new file:   django_wc/wc_app/views/homeView.py

--- django_wc/wc_app/urls.py
+++ django_wc/wc_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from wc_app.views import productView
+from wc_app.views import homeView
 
 
 urlpatterns = [
-    path('produto/', productView.ProductView, name='ProductView')
+    path('produto/', productView.ProductView, name='ProductView'),
+    path('', homeView.HomeView, name='HomeView')
 ]
