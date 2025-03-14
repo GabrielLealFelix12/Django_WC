647fb3f90d11d916ae836ac93bdc36b3ceb89ac9
gabriel <gabafp@hotmail.com>
2025-03-14 18:52:42 -0400

    modified:   django_wc/django_wc/settings.py 	new file:   django_wc/django_wc/templates/templateHome.html 	modified:   django_wc/wc_app/views/homeView.py

--- django_wc/wc_app/views/homeView.py
+++ django_wc/wc_app/views/homeView.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.template import loader
 
 def HomeView(request):
-    return HttpResponse("HomeView")
+      home = loader.get_template('templateHome.html')
+      return HttpResponse(home.render())
